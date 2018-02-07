d69f92bd83398bec0d2e8700efb6c9b496afcfa4
Exp <theexp111@gmail.com>
2018-02-07 14:43:17 +0100

    -Moved all screen related stuff to a new class -Added a Control Thread which works nicely

--- TS3/TS3-Plugin/src/config.h
+++ TS3/TS3-Plugin/src/config.h
@@ -1,5 +1,15 @@
 #pragma once
 
+#include <string>
+#include <vector>
+#include "teamspeak/public_errors.h"
+#include "teamspeak/public_errors_rare.h"
+#include "teamspeak/public_definitions.h"
+#include "teamspeak/public_rare_definitions.h"
+#include "teamspeak/clientlib_publicdefinitions.h"
+#include "ts3_functions.h"
+#include <LogitechLCDLib.h>
+
 class Config
 {
 public:
@@ -11,3 +21,46 @@ public:
 };
 
 extern Config* config;
+
+
+class LCDScreen
+{
+private:
+	bool gotMessage = false;
+	std::string newestMessage = "";
+	anyID messageSender = 0;
+	bool isActive = false;
+	unsigned cursorPosition = 0;
+	HANDLE controlThread = 0;
+	struct TS3Functions ts3Functions;
+
+public:
+	LCDScreen() {};
+	~LCDScreen()
+	{
+		if (isActive)
+			Shutdown();
+	}
+	
+	void Init();
+	void Shutdown();
+
+	void AddMessage(const char* msg, anyID sender);
+
+	unsigned GetPosition() const;
+	void ChangePosition(int changeValue);
+
+	bool IsActive() const { return isActive; }
+
+	void SetTS3Functions(const struct TS3Functions funcs);
+
+	void Update();
+
+	void StartControlThread(); //Check controls in another thread
+
+	//Helper
+	bool isClientInList(anyID* clientList, anyID* clientID);
+	std::vector<anyID> GetChannelContent(uint64 serverConnectionHandlerID, uint64 channelID);
+};
+
+extern LCDScreen* screen;
--- TS3/TS3-Plugin/src/plugin.cpp
+++ TS3/TS3-Plugin/src/plugin.cpp
@@ -99,6 +99,7 @@ const char* ts3plugin_description() {
 
 void ts3plugin_setFunctionPointers(const struct TS3Functions funcs) {
 	ts3Functions = funcs;
+	screen->SetTS3Functions(funcs);
 }
 
 int ts3plugin_init() {
@@ -112,14 +113,13 @@ int ts3plugin_init() {
 	ts3Functions.getConfigPath(configPath, PATH_BUFSIZE);
 	ts3Functions.getPluginPath(pluginPath, PATH_BUFSIZE, pluginID);
 
-	LogiLcdInit(_wcsdup(L"TeamSpeak 3"), LOGI_LCD_TYPE_MONO | LOGI_LCD_TYPE_COLOR);
-	LogiLcdUpdate();
+	screen->Init();
 
 	return 0;
 }
 
 void ts3plugin_shutdown() {
-	LogiLcdShutdown();
+	screen->Shutdown();
 	if (pluginID) {
 		free(pluginID);
 		pluginID = NULL;
@@ -199,187 +199,6 @@ void ts3plugin_initMenus(struct PluginMenuItem*** menuItems, char** menuIcon) {
 	_strcpy(*menuIcon, PLUGIN_MENU_BUFSZ, ""); //PLUGIN MENU IMAGE
 }
 
-bool isClientInList(anyID* clientList, anyID* clientID) 
-{
-	for (int i = 0; clientList[i]; i++)
-	{
-		//ts3Functions.printMessageToCurrentTab(std::string(std::to_string(clientList[i]) + ";" +std::to_string(*clientID) + "\n").c_str());
-		if (clientList[i] == *clientID) 
-			return true;
-	}
-
-	return false;
-}
-
-std::vector<anyID> GetChannelContent(uint64 serverConnectionHandlerID, uint64 channelID)
-{
-	std::vector<anyID> channelClientVector = std::vector<anyID>();
-
-	anyID* channelClientList;
-	ts3Functions.getChannelClientList(serverConnectionHandlerID, channelID, &channelClientList);
-
-	anyID* clientList;
-	if (ts3Functions.getClientList(serverConnectionHandlerID, &clientList) != ERROR_ok)
-		return channelClientVector;
-
-	int clientType;
-	for (int i = 0; clientList[i]; i++)
-	{
-		if (ts3Functions.getClientVariableAsInt(serverConnectionHandlerID, clientList[i], CLIENT_TYPE, &clientType) != ERROR_ok)
-			continue;
-
-		//if (clientType == 1) //Query
-		//	continue;
-
-		if (!isClientInList(channelClientList, &clientList[i]))
-			continue;
-
-		channelClientVector.push_back(clientList[i]);
-	}
-
-	return channelClientVector;
-}
-
-unsigned GetChannelContentCount(uint64 serverConnectionHandlerID, uint64 channelID)
-{
-	unsigned count = 0;
-
-	anyID* channelClientList;
-	ts3Functions.getChannelClientList(serverConnectionHandlerID, channelID, &channelClientList);
-
-	anyID* clientList;
-	ts3Functions.getClientList(serverConnectionHandlerID, &clientList);
-
-	int clientType;
-	for (int i = 0; clientList[i]; i++)
-	{
-		if (ts3Functions.getClientVariableAsInt(serverConnectionHandlerID, clientList[i], CLIENT_TYPE, &clientType) != ERROR_ok)
-			continue;
-
-		//if (clientType == 1) //Query
-		//	continue;
-
-		if (!isClientInList(channelClientList, &clientList[i]))
-			continue;
-
-		count++;
-	}
-
-	return count;
-}
-
-void Update()
-{
-	uint64 serverConnectionHandlerID = ts3Functions.getCurrentServerConnectionHandlerID();
-	anyID clientID = 0;
-	uint64 channelID = 0;
-	ts3Functions.getClientID(serverConnectionHandlerID, &clientID);
-	ts3Functions.getChannelOfClient(serverConnectionHandlerID, clientID, &channelID);
-
-	std::vector<anyID> channelClientList = GetChannelContent(serverConnectionHandlerID, channelID);
-
-	//Title (ex: TeamSpeak 3 - 3/15)
-	int talking, inputMuted, outputMuted;
-	ts3Functions.getClientSelfVariableAsInt(serverConnectionHandlerID, CLIENT_FLAG_TALKING, &talking);
-	ts3Functions.getClientSelfVariableAsInt(serverConnectionHandlerID, CLIENT_INPUT_MUTED, &inputMuted);
-	ts3Functions.getClientSelfVariableAsInt(serverConnectionHandlerID, CLIENT_OUTPUT_MUTED, &outputMuted);
-
-	int red = talking ? 255 : outputMuted ? 160 : inputMuted ? 0 : 255;
-	int green = talking ? 0 : outputMuted ? 160 : inputMuted ? 0 : 255;
-	int blue = talking ? 0 : outputMuted ? 160 : inputMuted ? 204 : 255;
-
-	unsigned channelClientCount = channelClientList.size();
-	int serverClientCount = 0;
-	ts3Functions.requestServerVariables(serverConnectionHandlerID); //we need to request for client count
-	ts3Functions.getServerVariableAsInt(serverConnectionHandlerID, VIRTUALSERVER_CLIENTS_ONLINE, &serverClientCount);
-	std::string title = "TS3 - " + std::to_string(channelClientCount) + "/" + std::to_string(serverClientCount);
-	LogiLcdColorSetTitle(_wcsdup(std::wstring(title.begin(), title.end()).c_str()), red, green, blue);
-
-	//ServerName (ex: MaCoGa - Dj's Kotstube)
-	char* serverName = new char[128];
-	char* channelName = new char[128];
-	std::wstring serverChannelNames = L"";
-	ts3Functions.getServerVariableAsString(serverConnectionHandlerID, VIRTUALSERVER_NAME, &serverName);
-	ts3Functions.getChannelVariableAsString(serverConnectionHandlerID, channelID, CHANNEL_NAME, &channelName);
-	std::string sServerName{ serverName };
-	if (sServerName.size() > 13) //30 is the maximum so resize it to have the '-' in the middle
-		sServerName.resize(13);
-	std::string sChannelName{ channelName };
-	serverChannelNames += std::wstring(sServerName.begin(), sServerName.end()) + L" - " + std::wstring(sChannelName.begin(), sChannelName.end());
-	LogiLcdColorSetText(0, _wcsdup(serverChannelNames.c_str()));
-
-	if (channelClientList.size() - config->pos < 7) //if people have left and we have space move the pos so the screen is full
-	{
-		config->pos -= (7 - (channelClientList.size() - config->pos));
-	}
-
-	if (config->pos > channelClientList.size())
-		config->pos = 0; //Better reset it if I fucked up
-
-	//Clients	//start at pos ; go till we hit the limit (no more clients/no more space)
-	//int position = 0;
-	for (unsigned i = config->pos; i < channelClientList.size() && i < config->pos + 7; i++)
-	{
-		//if (channelClientList[i] == clientID) //TODO
-		//	continue;
-		//position++;
-		char* clientName = new char[64];
-		int talking, inputMuted, outputMuted;
-		ts3Functions.getClientVariableAsString(serverConnectionHandlerID, channelClientList[i], CLIENT_NICKNAME, &clientName);
-
-		if (channelClientList[i] == clientID)
-		{
-			ts3Functions.getClientSelfVariableAsInt(serverConnectionHandlerID, CLIENT_FLAG_TALKING, &talking);
-			ts3Functions.getClientSelfVariableAsInt(serverConnectionHandlerID, CLIENT_INPUT_MUTED, &inputMuted);
-			ts3Functions.getClientSelfVariableAsInt(serverConnectionHandlerID, CLIENT_OUTPUT_MUTED, &outputMuted);
-		}
-		else
-		{
-			ts3Functions.getClientVariableAsInt(serverConnectionHandlerID, channelClientList[i], CLIENT_FLAG_TALKING, &talking);
-			ts3Functions.getClientVariableAsInt(serverConnectionHandlerID, channelClientList[i], CLIENT_INPUT_MUTED, &inputMuted);
-			ts3Functions.getClientVariableAsInt(serverConnectionHandlerID, channelClientList[i], CLIENT_OUTPUT_MUTED, &outputMuted);
-		}
-
-		int red = talking ? 255 : outputMuted ? 160 : inputMuted ? 0 : 255;
-		int green = talking ? 0 : outputMuted ? 160 : inputMuted ? 0 : 255;
-		int blue = talking ? 0 : outputMuted ? 160 : inputMuted ? 204 : 255;
-		//std::string status = inputMuted && outputMuted ? "<IO> " : inputMuted ? "<I> " : outputMuted ? "<O> " : "";
-		std::string text = std::string(clientName);
-		LogiLcdColorSetText(i + 1 - config->pos, _wcsdup(std::wstring(text.begin(), text.end()).c_str()), red, green, blue);
-	}
-
-	for (unsigned i = channelClientList.size(); i < 7; i++) //Empty the other lines
-	{
-		LogiLcdColorSetText(i + 1, _wcsdup(L""));
-	}
-
-	//Refresh Screen
-	LogiLcdUpdate();
-}
-
-void ChangePosition(int changeValue)
-{
-	int changed = config->pos + changeValue;
-	if (changed < 0)
-		return;
-
-	uint64 serverConnectionHandlerID = ts3Functions.getCurrentServerConnectionHandlerID();
-	anyID clientID = 0;
-	uint64 channelID = 0;
-	ts3Functions.getClientID(serverConnectionHandlerID, &clientID);
-	ts3Functions.getChannelOfClient(serverConnectionHandlerID, clientID, &channelID);
-
-	std::vector<anyID> channelClientList = GetChannelContent(serverConnectionHandlerID, channelID);
-
-	if (channelClientList.size() - changed <= 7) //if we have less than 7 people no need to change the pos
-		return;
-
-	config->pos = changed;
-
-	//Refresh
-	Update();
-}
-
 /************************** TeamSpeak callbacks ***************************/
 
 void ts3plugin_onMenuItemEvent(uint64 serverConnectionHandlerID, enum PluginMenuType type, int menuItemID, uint64 selectedItemID) {
@@ -391,13 +210,13 @@ void ts3plugin_onMenuItemEvent(uint64 serverConnectionHandlerID, enum PluginMenu
 			ts3Functions.printMessageToCurrentTab(std::to_string(config->testBool).c_str());
 			break;
 		case MENU_ID_GLOBAL_2:
-			Update();
+			screen->Update();
 			break;
 		case MENU_ID_GLOBAL_3:
-			ChangePosition(-1);
+			screen->ChangePosition(-1);
 			break;
 		case MENU_ID_GLOBAL_4:
-			ChangePosition(1);
+			screen->ChangePosition(1);
 			break;
 		default:
 			break;
@@ -410,22 +229,31 @@ void ts3plugin_onMenuItemEvent(uint64 serverConnectionHandlerID, enum PluginMenu
 
 void ts3plugin_onConnectStatusChangeEvent(uint64 serverConnectionHandlerID, int newStatus, unsigned int errorNumber)
 {
-	Update();
+	screen->Update();
 }
 
 void ts3plugin_onClientMoveEvent(uint64 serverConnectionHandlerID, anyID clientID, uint64 oldChannelID, uint64 newChannelID, int visibility, const char * moveMessage)
 {
-	Update();
+	screen->Update();
+}
+
+int ts3plugin_onTextMessageEvent(uint64 serverConnectionHandlerID, anyID targetMode, anyID toID, anyID fromID, const char * fromName, const char * fromUniqueIdentifier, const char * message, int ffIgnored)
+{
+	//Set message as newest message
+	screen->AddMessage(message, fromID);
+	//Update
+	screen->Update();
+	return 0;
 }
 
 void ts3plugin_onTalkStatusChangeEvent(uint64 serverConnectionHandlerID, int status, int isReceivedWhisper, anyID clientID)
 {
-	Update();
+	screen->Update();
 }
 
 void ts3plugin_onUpdateClientEvent(uint64 serverConnectionHandlerID, anyID clientID, anyID invokerID, const char* invokerName, const char* invokerUniqueIdentifier)
 {
-	Update();
+	screen->Update();
 }
 
 const char* ts3plugin_keyDeviceName(const char* keyIdentifier) {
